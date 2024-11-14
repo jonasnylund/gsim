5d67b2538f86f612bdba44b97e7bb32b24bd1155
Jonas Nylund <35763296+jonasnylund@users.noreply.github.com>
2024-11-14 21:46:21 +0100

    Add mouse controls to viewer

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -7,14 +7,23 @@ import pygame
 from gsim.model import py_model  # type: ignore
 
 
+def draw_circle_alpha(surface, color, center, radius):
+  target_rect = pygame.Rect(center, (0, 0)).inflate((radius * 2 + 1, radius * 2 + 1))
+  intermediate = pygame.Surface(target_rect.size, pygame.SRCALPHA)
+  pygame.draw.circle(intermediate, color, (radius + 1, radius + 1), radius)
+  surface.blit(intermediate, target_rect)
+
+
 class Viewer:
   """Viewer of particle clouds."""
 
   def __init__(self):
     self.display = pygame.display.set_mode(
       size=(1000, 500),
-      flags=pygame.RESIZABLE,
+      flags=pygame.RESIZABLE | pygame.DOUBLEBUF,
     )
+    self.scale: float = 1
+    self.center: tuple[float, float] = (0, 0)
     self.rendering_time: float = 0
 
   def __del__(self):
@@ -24,17 +33,20 @@ class Viewer:
     """Renders all the given particles to the display."""
     start_time = time.time()
     size = self.display.get_size()
-    center_x = size[0] / 2
-    center_y = size[1] / 2
+    center_x = size[0] / 2 - self.center[0]
+    center_y = size[1] / 2 - self.center[1]
 
     self.display.fill((0, 0, 0))
     for particle in particles:
       x, y = particle.position[:2]
-      pygame.draw.circle(
+      size = np.pow(particle.mass, 1 / 3) * 2 * self.scale
+      opacity = min(size, 2) / 2 * 255
+
+      draw_circle_alpha(
         self.display,
-        color=(200, 200, 255),
-        center=(x + center_x, y + center_y),
-        radius=np.ceil(np.sqrt(particle.mass)),
+        color=(200, 200, 255, int(opacity)),
+        center=(x * self.scale + center_x, y * self.scale + center_y),
+        radius=max(1, size),
       )
     pygame.display.flip()
 
@@ -46,6 +58,12 @@ class Viewer:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         stop = True
+      elif event.type == pygame.MOUSEWHEEL:
+        self.scale *= 1 + np.clip(event.y, -2, 2) / 10
+      elif event.type == pygame.MOUSEMOTION:
+        if event.buttons[0]:
+          x = self.center[0] - event.rel[0]
+          y = self.center[1] - event.rel[1]
+          self.center = (x, y)
 
-    pygame.event.clear()
     return not stop
